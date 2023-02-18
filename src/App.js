0ce97eb8ca337bda9a96437ef973b5c4a42bde8e
unknown <brosjayme@gmail.com>
2023-02-18 16:10:06 +0100

    my about update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import About from './Components/About';
 
 function App () {
   return (
     <div>
      < Navbar />
      <Home />
+     <About />
     </div>
   );
 }
