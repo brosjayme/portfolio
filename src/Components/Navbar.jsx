3dcf90d23e3b9e1d5c045fcc528d48109fef6fca
unknown <brosjayme@gmail.com>
2023-05-29 07:28:54 +0100

    removed the darktheme feature

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-} from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/logo.png';
-import Moon from '../assets/moon.png'
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/logo.png";
+// import Moon from "../assets/moon.png";
 // import Sun from '../assets/sun.png';
-import { Link } from 'react-router-dom';
-import '../index.css'
+import { Link } from "react-router-dom";
+import "../index.css";
 
 // let btn = document.getElementById('btn');
 // // let btnText = document.getElementById('btnText');
@@ -21,55 +16,45 @@ import '../index.css'
 //   document.body.classList.toggle('dark-theme')
 
 //   if(document.body.classList.contains('dark-theme')){
-//     btnIcon.src = {Sun} 
+//     btnIcon.src = {Sun}
 //   }
 // }
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
- 
 
   return (
-    <div className='fixed w-full h-[60px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className="fixed w-full h-[60px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt='Logo' style={{ width: '50px', height:'50px'}} />
+        <img src={Logo} alt="Logo" style={{ width: "50px", height: "50px" }} />
       </div>
 
       {/* menu */}
-      <ul className='hidden md:flex'>
+      <ul className="hidden md:flex">
         <li>
-          <Link to='/'>
-            Home
-          </Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/about'>
-            About
-          </Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to='/skills'>
-            Skills
-          </Link>
+          <Link to="/skills">Skills</Link>
         </li>
         <li>
-          <Link to="/portfolio">
-            Portfolio
-          </Link>
+          <Link to="/portfolio">Portfolio</Link>
         </li>
         <li>
-          <Link to='/contact' >
-            Contact
-          </Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      <div className='toggle-btn' id='btn'>
+      {/* darktheme */}
+      {/* <div className='toggle-btn' id='btn'>
         <span>Dark</span>
         <img src={Moon} alt='' id='btnIcon'/>
-      </div>
-    
+      </div> */}
+
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+      <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
@@ -77,72 +62,72 @@ const Navbar = () => {
       <ul
         className={
           !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className='py-6 text-4xl'>
-          <Link onClick={handleClick} to='/'>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="/">
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='/about'>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="/about">
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='/skills'>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="/skills">
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='/portfolio' >
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="/portfolio">
             Portfolio
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='/contact' >
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="/contact">
             Contact
           </Link>
         </li>
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
