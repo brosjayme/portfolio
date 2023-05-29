94e2902cff97b34a57833d5e748b08583db8a333
unknown <brosjayme@gmail.com>
2023-05-29 07:35:59 +0100

    removed the darktheme feature

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react';
-import NavBar from '../Components/Navbar';
+import React, { useRef } from "react";
+import NavBar from "../Components/Navbar";
 // import SocialLinks from '../Components/SocialLinks';
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 export const ContactUs = () => {
   const form = useRef();
@@ -9,34 +9,74 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_bbg6oui', 'template_vm445e4', e.target, 'mVDiOfpW5fQu4iGI9')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_bbg6oui",
+        "template_vm445e4",
+        e.target,
+        "mVDiOfpW5fQu4iGI9"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
+        }
+      );
+    e.target.reset();
   };
 
   return (
     <>
-      <NavBar/>
-     {/* <SocialLinks/> */}
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-1 bg-gradient-to-b from-black via-black to-gray-800'>
-    <form ref={form} onSubmit={sendEmail}  className='flex flex-col max-w-[450px] w-full'>
-        <div className='pb-4'>
-                <p className='text-3xl font-bold inline border-b-2 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-1'> Submit the form below or shoot me an email - okorojohn.ag@gmail.com</p>
-            </div>
-            <input className='bg-[#ccd6f6] p-1' type="text" placeholder='Name' name='name' required/>
-            <input className='my-2 p-1 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required />
-            <textarea className='bg-[#ccd6f6] p-1' name="message" rows="4" placeholder='Message' required></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-   
-    </form>
-   </div> 
-   
-   </>
+      <NavBar />
+      {/* <SocialLinks/> */}
+      <div
+        name="contact"
+        className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-1 bg-gradient-to-b from-black via-black to-gray-800"
+      >
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex flex-col max-w-[450px] w-full"
+        >
+          <div className="pb-4">
+            <p className="text-3xl font-bold inline border-b-2 border-pink-600 text-gray-300">
+              Contact
+            </p>
+            <p className="text-gray-300 py-1">
+              {" "}
+              Please fill the form below or shoot me an email -
+              okorojohn.ag@gmail.com
+            </p>
+          </div>
+          <input
+            className="bg-[#ccd6f6] p-1"
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+          />
+          <input
+            className="my-2 p-1 bg-[#ccd6f6]"
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <textarea
+            className="bg-[#ccd6f6] p-1"
+            name="message"
+            rows="4"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 my-8 mx-auto flex items-center">
+            Let's Collaborate
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
-export default ContactUs
+export default ContactUs;
