84507f8f41225a0814cf6d3aae17b3283009102f
unknown <brosjayme@gmail.com>
2023-08-03 09:34:20 +0100

    commit color change on skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -48,7 +48,7 @@ const Skills = () => {
               <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
               <p className="my-4">REACT</p>
             </div>
-            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 text-gray-100">
               <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
               <p className="my-4">GITHUB</p>
             </div>
